4b2be5ef82e8ba7f39a4b36e1eeecc121d4dc8e9
Tibor Šimko <tibor.simko@cern.ch>
2024-01-11 13:50:54 +0100

    ci(commitlint): addition of commit message linter (#33)
    Adds commitlint to check the commit message style against agreed
    conventional commits configuration.

---
+++ run-tests.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+#
+# This file is part of REANA.
+# Copyright (C) 2024 CERN.
+#
+# REANA is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+set -o errexit
+set -o nounset
+
+check_commitlint () {
+    from=${2:-master}
+    to=${3:-HEAD}
+    npx commitlint --from="$from" --to="$to"
+    found=0
+    while IFS= read -r line; do
+        if echo "$line" | grep -qP "\(\#[0-9]+\)$"; then
+            true
+        else
+            echo "✖   PR number missing in $line"
+            found=1
+        fi
+    done < <(git log "$from..$to" --format="%s")
+    if [ $found -gt 0 ]; then
+        exit 1
+    fi
+}
+
+check_shellcheck () {
+    find . -name "*.sh" -exec shellcheck {} \;
+}
+
+check_all () {
+    check_commitlint
+    check_shellcheck
+}
+
+if [ $# -eq 0 ]; then
+    check_all
+    exit 0
+fi
+
+arg="$1"
+case $arg in
+    --check-commitlint) check_commitlint "$@";;
+    --check-shellcheck) check_shellcheck;;
+    *) echo "[ERROR] Invalid argument '$arg'. Exiting." && exit 1;;
+esac
